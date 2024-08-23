653bc40f1439a39935cdd418f3b088ee8b8a1df3
ashram7 <ashram7@hanmail.net>
2024-08-23 17:45:47 +0900

    헤로쿠 서버 정지시키고 part1_end로 다시 세팅3

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -82,8 +82,9 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        config.setAllowedOrigins(Arrays.asList(
+                "https://carproject2-1788344c9c9e.herokuapp.com:3000"
+        ));
         config.setAllowedMethods(Arrays.asList("*"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(false);
