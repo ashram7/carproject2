cf34716e9cd4641ea89a9e176ceec8e54e874a79
ashram7 <ashram7@hanmail.net>
2024-08-24 13:50:28 +0900

    깃허브에서 작동되는 부분 다운로드 받아서 테스트

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -39,19 +39,30 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // /login 경로에 대한 POST 요청은 인증 없이 허용
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()) // 그 외 모든 요청은 인증 필요
                 .exceptionHandling(e -> e.authenticationEntryPoint(exceptionHandler))
                 .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+    /*@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz  //설명1)
+                        .anyRequest().authenticated())
+                .formLogin(form -> form  //설명2)
+                        .loginPage("/login").permitAll())
+                // Updated way to enable HTTP Basic authentication
+                .httpBasic(Customizer.withDefaults());  //설명3)
+
+        return http.build();
+    }*/
 
     /*@Bean
     public UserDetailsService userDetailsService() {
         UserDetails user =
-                // User.builder()
                 User.withDefaultPasswordEncoder()
                         .username("user")
-                        .password("{noop}password")  // Use {noop} for plain text (for example purposes only)
+                        .password("{noop}password")
                         .roles("USER")
                         .build();
 
@@ -83,8 +94,9 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList(
-                "https://carproject2-1788344c9c9e.herokuapp.com:3000"
+                "https://carproject2-6d0fd6cfe89f.herokuapp.com:3000"
         ));
+        //config.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
         config.setAllowedMethods(Arrays.asList("*"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(false);
